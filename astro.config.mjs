50b0a3e0757061113dde88ae45414a011bdd97ec
Lloyd Atkinson <lloydjatkinson@gmail.com>
2022-10-13 13:35:49 +0100

    chore: setup of initial components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), preact()]
+});
